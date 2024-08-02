367c750d3de5f8cdbae6c804598161bc798dbcc8
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-08-02 22:21:00 +0100

    chore: Remove unused files and update dependencies

--- android/app/src/main/kotlin/com/example/soko_aerial_interns_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/soko_aerial_interns_app/MainActivity.kt
@@ -2,6 +2,4 @@ package com.example.soko_aerial_interns_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity(){
-    
-}
+class MainActivity: FlutterActivity()
